5544f62a24a80001712fe08857cce87da6adc396
Simon Goričar <simon.peter.goricar@gmail.com>
2023-07-06 00:09:06 +0200

    Add non-exclusive variants of queueing scripts

---
+++ scripts/arnes/queue_04vs_14400f-20w_dynamic.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -e
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+echo "Script resides in $SCRIPT_DIR."
+
+"$SCRIPT_DIR/queue-job.sh" \
+  --jobFile="blender-projects/04_very-simple/04_very-simple_measuring_14400f-20w_dynamic.toml" \
+  --resultsDirectory="blender-projects/04_very-simple/results" \
+  --runName="04vs_14400f-20w_dynamic" \
+  --runPort="9932" \
+  --runTimeLimitMinutes=400 \
+  --numWorkers=20

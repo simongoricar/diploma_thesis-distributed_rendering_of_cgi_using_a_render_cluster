93a727c3c234aaaf5d6c2f85aeba1b323e315548
Simon Goričar <simon.peter.goricar@gmail.com>
2023-08-29 18:17:17 +0200

    Update `view-queue.sh`

--- scripts/arnes/view-queue.sh
+++ scripts/arnes/view-queue.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -e
-squeue --me -o "%.18i %.9P %.56j %.8u %.2t %.10M %24S %44Y %R" --sort=S
+squeue --me -o "%.18i %.9P | %.40j %.8u %.2t | %.11M %.11l %.20S | %.4C %.9f %.23E %R" --sort=S
